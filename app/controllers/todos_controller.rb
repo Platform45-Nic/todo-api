5adb1ee32f73829c54deee72feb6edd4107f7680
Platform45-Nic <nicolo@platform45.com>
2019-08-26 19:12:16 +0200

    Added RESTful to todos controller. Set up FactoryBot and rspec and all but 1 rspec tests are passing. Will investigate and fix.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,42 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :update, :destroy]
+
+  # Get
+  def index
+    @todos = Todo.all
+    json_response(@todos)
+  end
+
+  # POST
+  def create
+    @todo = Todo.create!(todo_params)
+    json_response(@todo, :created)
+  end
+
+  # GET
+  def show
+    json_response(@todo)
+  end
+
+  # PUT
+  def update
+    @todo.update(todo_params)
+    head :no_content
+  end
+
+  # DELETE
+  def destroy
+    @todo.destroy
+    head :no_content
+  end
+
+  private
+
+  def todo_params
+    params.permit(:title, :created_by)
+  end
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
 end
--- spec/support/request_spec_helper.rb
+++ spec/support/request_spec_helper.rb
@@ -1,5 +1,5 @@
 # spec/support/request_spec_helper
-module request_spec_helper
+module RequestSpecHelper
   # Parse JSON response to ruby hash
   def json
     JSON.parse(response.body)
