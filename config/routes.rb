e6f97e1df8a16da0c17bf584e8637a4564f2a2bb
Platform45-Nic <nicolo@platform45.com>
2019-08-28 14:33:00 +0200

    Added route for signup users#create.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   end
 
   post 'auth/login', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
