092ee41e5f9225c1e3216d625146d7a0d2f7fd14
Platform45-Nic <nicolo@platform45.com>
2019-09-06 14:59:03 +0200

    Added v2 controllers and routing scope.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  scope module: :v2, constraints: ApiVersion.new('v2') do
+    resources :todos, only: :index
+  end
+
   # Namespace the controllers without affecting the URI
   scope module: :v1, constraints: ApiVersion.new('v1', true) do
     resources :todos do
